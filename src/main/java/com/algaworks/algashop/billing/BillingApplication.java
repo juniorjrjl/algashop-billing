7560d62a0979264e20fda460748c3c290b33ed87
Junior <junior.jr.jl@gmail.com>
2026-04-10 11:05:26 -0300

    feat: improvements and wiremock config

--- src/main/java/com/algaworks/algashop/billing/BillingApplication.java
+++ src/main/java/com/algaworks/algashop/billing/BillingApplication.java
@@ -3,10 +3,14 @@ package com.algaworks.algashop.billing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BillingApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneOffset.UTC));
 		SpringApplication.run(BillingApplication.class, args);
 	}
 
