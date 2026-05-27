515b7fbc921d10305ca1a4de3c715d5661412e59
Junior <junior.jr.jl@gmail.com>
2026-05-27 16:19:17 -0300

    feat: dependencies sort

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	idea
-	java
-	jacoco
 	alias(libs.plugins.spring.boot)
 	alias(libs.plugins.spring.dependency.management)
+	idea
+	jacoco
+	java
 }
 
 group = "com.algaworks.algashop"
@@ -28,45 +28,42 @@ repositories {
 }
 
 dependencies {
-	// IMPLEMENTATION
-	implementation(libs.java.uuid.generator)
-	implementation(libs.commons.validator)
-	implementation(libs.commons.lang3)
-	implementation(libs.mapstruct)
-	implementation(libs.spring.boot.starter.data.jpa)
-	implementation(libs.spring.boot.starter.webmvc)
-	implementation(libs.spring.boot.starter.flyway)
-	implementation(libs.spring.boot.starter.validation)
-	implementation(libs.spring.boot.starter.restclient)
-	implementation(libs.flyway.database.postgresql)
 
-	// COMPILE ONLY & RUNTIME ONLY
-	compileOnly(libs.lombok)
-	runtimeOnly(libs.postgresql)
-
-	// ANNOTATION PROCESSOR
-	annotationProcessor(libs.mapstruct.processor)
+	annotationProcessor(libs.hibernate.processor)
 	annotationProcessor(libs.lombok)
 	annotationProcessor(libs.lombok.mapstruct.binding)
-	annotationProcessor(libs.hibernate.processor)
+	annotationProcessor(libs.mapstruct.processor)
 
-	// TEST COMPILE & ANNOTATION PROCESSOR
+	compileOnly(libs.lombok)
+
+	implementation(libs.commons.lang3)
+	implementation(libs.commons.validator)
+	implementation(libs.mapstruct)
+	implementation(libs.flyway.database.postgresql)
+	implementation(libs.java.uuid.generator)
+	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.spring.boot.starter.flyway)
+	implementation(libs.spring.boot.starter.restclient)
+	implementation(libs.spring.boot.starter.validation)
+	implementation(libs.spring.boot.starter.webmvc)
+
+	mockitoAgent(libs.mockito.core) {
+		isTransitive = false
+	}
+
+	runtimeOnly(libs.postgresql)
+
 	testCompileOnly(libs.lombok)
 
 	testAnnotationProcessor(libs.lombok)
 
-	// TEST IMPLEMENTATION
-	testImplementation(libs.spring.boot.starter.flyway.test)
-	testImplementation(libs.datafaker)
 	testImplementation(libs.assertj.core)
+	testImplementation(libs.datafaker)
 	testImplementation(libs.spring.boot.starter.data.jpa.test)
+	testImplementation(libs.spring.boot.starter.flyway.test)
 	testImplementation(libs.spring.boot.starter.webmvc.test)
 	testImplementation(libs.wiremock.spring.boot)
 
-	// MOCKITO AGENT & RUNTIME
-	mockitoAgent(libs.mockito.core) {
-		isTransitive = false
-	}
 	testRuntimeOnly(libs.junit.platform.launcher)
 }
 
